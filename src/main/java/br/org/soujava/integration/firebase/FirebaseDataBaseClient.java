e702e8829b58d42ebfa14473effa101b09214769
Fernando Arruda <progfer@gmail.com>
2017-10-31 20:50:07 -0200

    - Integrando Google Firebase

--- src/main/java/br/org/soujava/integration/firebase/FirebaseDataBaseClient.java
+++ src/main/java/br/org/soujava/integration/firebase/FirebaseDataBaseClient.java
@@ -1,9 +1,17 @@
 package br.org.soujava.integration.firebase;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.Serializable;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Named;
+
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.auth.FirebaseCredentials;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 /**
  * Firebase Database Realtime Client
@@ -13,6 +21,27 @@ import javax.inject.Named;
  */
 @Named
 @ApplicationScoped
-public class FirebaseDataBaseClient implements Serializable{
+public class FirebaseDataBaseClient implements Serializable {
+
+	private final FirebaseDatabase database;
+
+	public FirebaseDataBaseClient() throws IOException {
+		FileInputStream serviceAccount = new FileInputStream(
+				"/Users/arruda/Documents/soujava-rio/token/soujava-rio-firebase-adminsdk-w0w8c-f9625b63c9.json");
+
+		FirebaseOptions options = new FirebaseOptions.Builder()
+				.setCredential(FirebaseCredentials.fromCertificate(serviceAccount))
+				.setDatabaseUrl("https://soujava-rio.firebaseio.com").build();
+
+		FirebaseApp.initializeApp(options);
+
+		database = FirebaseDatabase.getInstance();
+
+	}
+
+	public DatabaseReference getSlidesRepository() {
+		DatabaseReference ref = database.getReference("slides");
+		return ref;
+	}
 
 }
--- src/main/java/br/org/soujava/service/SlideService.java
+++ src/main/java/br/org/soujava/service/SlideService.java
@@ -16,8 +16,14 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
+
 import br.org.soujava.domain.Slide;
 import br.org.soujava.integration.firebase.FirebaseDataBaseClient;
+import lombok.extern.slf4j.Slf4j;
 
 /**
  * Slide Services
@@ -27,35 +33,51 @@ import br.org.soujava.integration.firebase.FirebaseDataBaseClient;
  */
 @Named
 @ApplicationScoped
-public class SlideService implements Serializable{
+@Slf4j
+public class SlideService implements Serializable {
 
 	@Inject
 	private FirebaseDataBaseClient firebaseDataBaseClient;
-	
+
 	private Map<String, Slide> map = new HashMap<>();
-	
+
 	public SlideService() {
-		this.map.put("AACDE221", Slide.builder().id("AACDE221").title("Title Slide 01").description("Slide 01 Desc").build());
+		this.map.put("AACDE221",
+				Slide.builder().id("AACDE221").title("Title Slide 01").description("Slide 01 Desc").build());
 	}
-	
-	public List<Slide> findAll(){
+
+	public List<Slide> findAll() {
+		DatabaseReference slidesRepository = firebaseDataBaseClient.getSlidesRepository();
+		// Attach a listener to read the data at our posts reference
+		slidesRepository.addValueEventListener(new ValueEventListener() {
+			@Override
+			public void onDataChange(DataSnapshot dataSnapshot) {
+				//Post post = dataSnapshot.getValue(Post.class);
+				System.out.println(dataSnapshot.getValue());
+			}
+
+			@Override
+			public void onCancelled(DatabaseError databaseError) {
+				System.out.println("The read failed: " + databaseError.getCode());
+			}
+		});
 		return new ArrayList<>(map.values());
 	}
-	
-	public Slide findbyId(String id){
+
+	public Slide findbyId(String id) {
 		return map.get(id);
 	}
-	
-	public void save(Slide slide){
+
+	public void save(Slide slide) {
 		map.put(slide.getId(), slide);
 	}
-	
-	public void update(Slide slide){
+
+	public void update(Slide slide) {
 		map.put(slide.getId(), slide);
 	}
-	
-	public void delete(String id){
+
+	public void delete(String id) {
 		map.remove(id);
 	}
-	
+
 }
